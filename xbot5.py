cedb4e2be1688d050b99149eef18c452bbbc8f5f
LudiqHacker <125390351+LudiqHacker@users.noreply.github.com>
2024-01-12 11:39:25 +0200

    Create xbot5.py

---
+++ xbot5.py
@@ -0,0 +1,92 @@
+import random
+from termcolor import colored
+import os
+import time
+os.system("clear")
+
+x1 = "__   ___           _   "
+x2 = "\ \ / / |         | |  "
+x3 = " \ V /| |__   ___ | |_ "
+x4 = "  > < | '_ \ / _ \| __|"
+x5 = " / . \| |_) | (_) | |_ "
+x6 = "/_/ \_\_.__/ \___/ \__|"
+x7 = "     By Hacker4o68     "
+
+print(colored(x1, "green"))
+print(colored(x2, "green"))
+print(colored(x3, "green"))
+print(colored(x4, "green"))
+print(colored(x5, "green"))
+print(colored(x6, "green"))
+print(colored(x7, "green"))
+
+print("⠀ ⠀ ⠀ ⠀ ⠀  ")
+
+bad_words = ["pederas", "grozen ", "gay", "gey", "maika ti", "mangal", "debel", "eba"]                                                        bot_name = "xbot"
+name = input("Whats your name: ")
+if name == "Netovski":
+  time.sleep(1.5)
+  print("Wait...")
+  time.sleep(1.5)
+  print("You are the chosen one!")
+  exit()
+else:
+  for i in range(100000):
+    smg = random.randint(1,7)
+    text = input(name + ">")
+    if text == "faster":
+      time.sleep(0.5)
+      print("motor brum brum")
+    else:
+      if text == "s kakvo se zanimavash":
+        time.sleep(0.5)
+        print("da eba maika ti")
+      else:
+          if text == "!good_mode=True":
+            time.sleep(0.5)
+            for i in range(100000):
+              smg1 = random.randint(1,7)
+              good_text = input(name + ">")
+              if smg1 == 1:
+                time.sleep(0.5)
+                print(bot_name + ">" +  "dobro utro")
+              if smg1 == 2:
+                time.sleep(0.5)
+                print(bot_name + ">" + "dobur den")
+              if smg1 == 3:
+                time.sleep(0.5)
+                print(bot_name + ">" + "ti si mi priqtel")
+              if smg1 == 4:
+                time.sleep(0.5)
+                print(bot_name + ">" + "obicham te")
+              if smg1 == 5:
+                time.sleep(0.5)
+                print(bot_name + ">" + "zdravei")
+              if smg1 == 6:
+                time.sleep(0.5)
+                print(bot_name + ">" + "imash hubavo ime")
+              if smg1 == 7:
+                time.sleep(0.5)
+                print(bot_name + ">" + "blagodarq")
+          else:
+            if smg == 1:
+              time.sleep(0.5)
+              print(bot_name + ">" + "Ebi se")
+            if smg == 2:
+              time.sleep(0.5)
+              print(bot_name + ">" + "tupa putko")
+            if smg == 3:
+              time.sleep(0.5)
+              print(bot_name + ">" + "Shut up nigga")
+            if smg == 4:
+              time.sleep(0.5)
+              print(bot_name + ">" + "mlukvai pedal")
+            if smg == 5:
+              time.sleep(0.5)
+              print(bot_name + ">" + "maika ti")
+            if smg == 6:
+              time.sleep(0.5)
+              print(bot_name + ">" + "mlukvai be " + name)
+            if smg == 7:
+              time.sleep(0.5)
+              print(bot_name + ">" + "Koi te pita?")
